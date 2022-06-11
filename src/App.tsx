3693084933bf6f79a672c0a023348f7f2eb2208c
JohnMoody <105369002+john082Coder@users.noreply.github.com>
2022-06-11 18:36:17 +0900

    update phantom wallet connection

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Connect2Phantom from './components/Connect2Phantom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        
+        <Connect2Phantom/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
